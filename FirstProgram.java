ec304e4312feb708509791fb2418499acddc10cb
Asmii Bhati <asmiibhati@gmail.com>
2026-02-16 19:18:58 +0530

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,11 @@
+public class FirstProgram
+{
+    public static void main(String[] args) 
+    {   //type 'sout' then hit tab, System.out.println(); will be shown automatically(VS code feature)
+        //type 'psvm' then hot tab, System.out.println(); will be shown automatically(VS code feature)
+        System.out.println("Hello World!"); //println() works same as using \n at the end of the string.
+        System.out.print("I like pizza!");
+        System.out.print("I like pasta too!\n");
+        System.out.print("Byee");
+    }
+}
