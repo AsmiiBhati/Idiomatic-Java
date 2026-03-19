22cdba2f5656d2c14b35665d2b267fa74014a681
Asmii Bhati <asmiibhati@gmail.com>
2026-03-19 10:56:11 +0530

    Add files via upload

---
+++ NumberTriangle1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class NumberTriangle1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter rows: ");
+        int row = sc.nextInt();
+        Triangle(row);
+        sc.close();
+    }
+    public static void Triangle(int row)
+    {
+        int i,j;
+        for(i=1;i<=row;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
